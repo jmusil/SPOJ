d99b7fe523d4eca4cc3034f0e11c530b13c90391
f3z0j <josef.musil@gmail.com>
2012-09-16 00:01:31 +0200

    Initial commit. Solutions for the SPOJ programming challenge problems.

---
+++ SPOJ11_FCTRL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace SPOJ11_FCTRL
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            UInt64 amountOfNumbers = Convert.ToUInt64(Console.ReadLine());
+            UInt64[] numbers = new UInt64[amountOfNumbers];
+
+            for (UInt64 i = 0; i < amountOfNumbers; i++)
+            {
+                numbers[i] = Convert.ToUInt64(Console.ReadLine());
+            }
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(getNumberOfTrailingZeroes(number));
+            }
+
+            return 0;
+        }
+
+        static UInt64 getNumberOfTrailingZeroes(UInt64 input)
+        {
+            UInt64 zeroes = 0;
+            double result = input;
+            while (result >= 1)
+            {
+                result = result / 5;
+                if (result >= 1)
+                {
+                    zeroes += (UInt64)Math.Truncate(result);
+                }
+            }
+            return zeroes;
+        }
+    }
+}
---
+++ SPOJ42_ADDREV/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPOJ42_ADDREV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UInt64 param = 0;
+            string input = "a";
+            bool firstRun = true;
+            while ((input = Console.ReadLine()) != null)
+            {
+                if (firstRun == false)
+                {
+                    string[] two_ints = input.Split(' ');
+
+                    UInt64[] numbers = new UInt64[2];
+                    numbers[0] = UInt64.Parse(two_ints[0]);
+                    numbers[1] = UInt64.Parse(two_ints[1]);
+
+                    Console.WriteLine(reverseNumber(reverseNumber(numbers[0]) + reverseNumber(numbers[1])));    
+                }
+                firstRun = false;
+            }
+        }
+
+        static UInt64 reverseNumber(UInt64 number)
+        {
+            string temp = number.ToString();
+            string reversed_temp = new string(temp.ToCharArray().Reverse().ToArray());
+            return UInt64.Parse(reversed_temp);
+        }
+        
+    }
+}
