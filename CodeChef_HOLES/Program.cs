0449b7cca520f8cd050b74f1eecd62a179748392
f3z0j <josef.musil@gmail.com>
2012-11-06 20:48:22 +0100

    Add solution for HOLES: http://www.codechef.com/problems/HOLES

---
+++ CodeChef_HOLES/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChef_HOLES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testcases = Int32.Parse(Console.ReadLine());
+            
+            while (testcases > 0)
+            {
+                string input = Console.ReadLine();
+                char[] line = new char[input.Length];
+                line = input.ToArray<char>();
+                int numberOfHoles = 0;
+                foreach (char letter in line)
+                {
+                    numberOfHoles += GetHoles(letter);
+                }
+                Console.WriteLine(numberOfHoles);
+                testcases--;
+            }
+        }
+
+        static int GetHoles(char c)
+        {
+            switch (c)
+            {
+                case 'A':
+                case 'D':
+                case 'O':
+                case 'P':
+                case 'Q':
+                case 'R':
+                    return 1;
+                case 'B':
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
