3f9d9e651766ae1c2baa950f17445e5c43fa9a72
f3z0j <josef.musil@gmail.com>
2013-02-08 00:20:36 +0100

    Add solution for MUFFINS3
    http://www.codechef.com/problems/MUFFINS3/

---
+++ CodeChef_MUFFINS3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChef_MUFFINS3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTestcases = Int32.Parse(Console.ReadLine());
+
+            while (numberOfTestcases-- > 0)
+            {
+                Console.WriteLine(Int32.Parse(Console.ReadLine())/2+1);
+            }
+        }
+    }
+}
