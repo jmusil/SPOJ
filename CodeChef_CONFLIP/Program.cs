cec61f8b03099313d63d8a77723a97cd77f5ab57
f3z0j <josef.musil@gmail.com>
2013-03-05 18:19:57 +0100

    Add solution for CONFLIP
    http://www.codechef.com/problems/CONFLIP

---
+++ CodeChef_CONFLIP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChef_CONFLIP
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int numberOfTestcases = Int32.Parse(Console.ReadLine());
+            int numberOfGames;
+            string[] input;
+            int initialFace;
+            Int64 numberOfCoins;
+            int faceToCheck;
+            
+
+            while (numberOfTestcases-- > 0)
+            {
+                numberOfGames = Int32.Parse(Console.ReadLine());
+                while (numberOfGames-- > 0)
+                {
+                    input = Console.ReadLine().Split(' ');
+                    initialFace = (Int32.Parse(input[0]) == 1) ? 1 : -1;
+                    numberOfCoins = Int64.Parse(input[1]);
+                    faceToCheck = (Int32.Parse(input[2]) == 1) ? 1 : -1;
+                    
+                    if (numberOfCoins % 2 == 0 || initialFace == faceToCheck)
+                    {
+                        Console.WriteLine(numberOfCoins / 2);
+                    }
+                    else
+                    {
+                        Console.WriteLine(numberOfCoins / 2 + 1);
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
