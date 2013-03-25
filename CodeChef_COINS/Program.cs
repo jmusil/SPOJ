8ecf392641ed713c6ebd22cdf234f6ed340b2858
jmusil <josef.musil@gmail.com>
2013-03-25 18:24:37 +0100

    Add solution for COINS
    http://www.codechef.com/problems/COINS/

---
+++ CodeChef_COINS/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChef_COINS
+{
+    class Program
+    {
+        static Dictionary<Int64, Int64> memoizedResults = new Dictionary<Int64, Int64>();
+        static Int64 temp;
+        static void Main(string[] args)
+        {
+            Int64 input;
+            Int64 converted;
+            
+            for (int i = 0; i < 10; i++)
+            {
+                input = Int32.Parse(Console.ReadLine());
+                converted = getMax(input);
+                if (converted > input)
+                {
+                    Console.WriteLine(converted);
+                }
+                else
+                {
+                    Console.WriteLine(input);
+                }
+            }
+        }
+
+        static Int64 getMax(Int64 value)
+        {
+            if (value == 0)
+            {
+                return 0;
+            }
+            if (memoizedResults.ContainsKey(value))
+            {
+                temp =  memoizedResults[value];
+            }
+            else
+            {
+                temp = (getMax(value / 2) + getMax(value / 3) + getMax(value / 4));
+                memoizedResults.Add(value, temp);
+            }
+
+            if (value < temp)
+            {
+                return temp;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
