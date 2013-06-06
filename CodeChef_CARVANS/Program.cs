3f5d758b288ef5d0b6acf7e1b601a3875bad542b
jmusil <josef.musil@gmail.com>
2013-06-06 14:11:27 +0200

    Add solution for CARVANS
    http://www.codechef.com/problems/CARVANS

---
+++ CodeChef_CARVANS/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChef_CARVANS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region variable declarations
+            int testcases = Int32.Parse(Console.ReadLine());
+            int cars;
+            string input;            
+            int[] speed;
+            int speedlimit;
+            int maxSpeedCars;
+            #endregion
+
+            for (int i = 0; i < testcases; i++)
+            {
+                maxSpeedCars = 1;
+                cars = Int32.Parse(Console.ReadLine());
+                input = Console.ReadLine();
+                
+                speed = input.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+                speedlimit = speed[0];
+                for (int j = 1; j < speed.Length; j++)
+                {
+                    if (speed[j] <= speedlimit)
+                    {
+                        speedlimit = speed[j];
+                        maxSpeedCars++;
+                    }
+                }
+                Console.WriteLine(maxSpeedCars);
+            }
+ 
+        }
+    }
+}
