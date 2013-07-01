ca2096dadf4bfff3039c2f367e11aa2b17060450
jmusil <josef.musil@gmail.com>
2013-07-01 11:11:39 +0200

    Add solution for DOUBLE
    http://www.codechef.com/problems/DOUBLE/

---
+++ CodeChef_DOUBLE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CodeChef_DOUBLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testcases = Int32.Parse(Console.ReadLine());
+            int length;
+            while (testcases-- > 0)
+            {
+                length = Int32.Parse(Console.ReadLine());
+                if (length == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else if (length % 2 == 0)
+                {
+                    Console.WriteLine(length);
+                }
+                else
+                {
+                    Console.WriteLine(length - 1);
+                }
+            }
+        }
+    }
+}
