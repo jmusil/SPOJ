d12a0f7dd4b035acc1a9f4aeca644e48d6e3e97d
jmusil <josef.musil@gmail.com>
2013-12-01 18:07:25 +0100

    Add solution for CIELAB
    http://www.codechef.com/problems/CIELAB/

---
+++ CodeChef_CIELAB/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChef_CIELAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(i => Int32.Parse(i)).ToArray();
+            
+            int result = input[0] - input[1];
+            if (result % 10 == 9)
+            {
+                result = result - 1;
+            }
+            else
+            {
+                result = result + 1;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
