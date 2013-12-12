a1db7792ba3cb2d5c8f12745ffc0e5ff2eb86d9c
jmusil <josef.musil@gmail.com>
2013-12-12 10:35:38 +0100

    Add solution for HORSES
    http://www.codechef.com/problems/HORSES/

---
+++ CodeChef_HORSES/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChef_HORSES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testcases = Int32.Parse(Console.ReadLine());
+            while (testcases-- > 0)
+            {
+                int horseCount = Int32.Parse(Console.ReadLine());
+                
+                int diff = 0;
+                List<int> skills = Console.ReadLine().Split(' ').Select(s => Int32.Parse(s)).ToList();
+                skills.Sort();
+                skills.Reverse();
+                int min = skills[0];
+                for (int i = 0; i < skills.Count - 1; i++)
+                {
+                    diff = skills[i] - skills[i + 1];
+                    if (diff < min)
+                    {
+                        min = diff;
+                    }
+                }
+                Console.WriteLine(min);
+            }
+        }
+    }
+}
