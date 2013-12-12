c80f5cf831c709fa47065bd65ada26e94a78ac53
jmusil <josef.musil@gmail.com>
2013-12-12 02:37:42 +0100

    Add solution for STONES
    http://www.codechef.com/problems/STONES/

---
+++ CodeChef_STONES/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChef_STONES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testcases = Int32.Parse(Console.ReadLine());
+            string jewels;
+            string jewelsRaw;
+            string minedStones;
+            int jewelsCount;
+            while (testcases-- > 0)
+            {
+                jewelsCount = 0;
+                jewelsRaw = Console.ReadLine();
+                minedStones = Console.ReadLine();
+
+                StringBuilder foo = new StringBuilder();
+                foreach (var character in jewelsRaw)
+                {
+                    if (!foo.ToString().Contains(character))
+                    {
+                        foo.Append(character);
+                    }
+                }
+                jewels = jewelsRaw.ToString();
+                foreach (var character in minedStones)
+                {
+                    if (jewels.Contains(character))
+                    {
+                        jewelsCount++;
+                    }
+                }
+                Console.WriteLine(jewelsCount);
+            }
+        }
+    }
+}
